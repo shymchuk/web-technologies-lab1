702ea30fb79f8aade6cfd1752f3e91373b6f4009
Tymofii Shymchuk <tymofii.shymchuk@gmail.com>
2022-07-05 20:50:32 +0300

    Implement registration and login

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sex = models.BooleanField()
+    birth = models.DateField()
+
+
+class PhoneNumber(models.Model):
+    number = models.TextField(
+        unique=False
+    )
+
+    class Meta:
+        ordering = ('-number',)
+
+
+class Person:
+    surname = models.TextField(
+        unique=True
+    )
+    phone = models.ManyToManyField(PhoneNumber)
+
+    class Meta:
+        ordering = ('-surname', )
--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.http import *
 
-# Create your views here.
+import json
+
+from .models import Profile
+
+
+@csrf_exempt
+def register_user(request: HttpRequest):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user = User.objects.create(
+            username=data['surname'],
+            email=data['email'],
+            password=data['password']
+        )
+        Profile.objects.create(
+            user=user,
+            sex=data['sex'] == 'male',
+            birth=data['birth']
+        )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+
+
+@csrf_exempt
+def login_user(request: HttpRequest):
+    if request.method == 'POST':
+        for i in request.POST.items():
+            print(i)
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse()
+        else:
+            return HttpResponseForbidden()
+    else:
+        return HttpResponseBadRequest()
+
+
